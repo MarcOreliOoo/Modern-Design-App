98a832ca4b8f7fbfc657254ea24a6bea2c337db6
MarcOreliOoo <albeaurelien@gmail.com>
2022-08-30 16:26:19 +0200

    init: navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,11 +1,34 @@
+import { useState } from 'react';
+import { close, logo, menu } from '../assets';
+import { navLinks } from '../constants';
+
 import { HiMenuAlt2 } from 'react-icons/hi';
 import { AiOutlineClose } from 'react-icons/ai';
 
-import logo from '../assets/react.svg';
 
 const Navbar = () => {
 	return (
-		<nav className="w-full flex justify-start xl:justify-center items-center p-1">
+		<nav className="w-full flex py-6 justify-center items-center navbar">
+			<img src={logo} alt="blabla" className="w-[1024px] h-[32px]" />
+			<ul className="list-none sm:flex hidden justify-end items-center flex-1">
+				{navLinks.map((nav,index) => (
+					<li 
+					key={nav.id}
+					className={'font-poppins font-normal cursor-pointer text-[16px] text-white'}>
+						<a href={`${nav.id}`}>
+							{nav.title}
+						</a>
+					</li>
+				))}
+			</ul>
+		</nav>	
+	);
+}
+
+export default Navbar;
+
+/**
+ * <nav className="w-full flex justify-start xl:justify-center items-center p-1">
 			<div className="md:flex-[0.5] flex-initial justify-center items-center">
 				<img src={logo} alt="logo" className="cursor-pointer"/>
 			</div>
@@ -13,7 +36,4 @@ const Navbar = () => {
 				
 			</ul>
 		</nav>
-	);
-}
-
-export default Navbar;
+ */
---
+++ client/tailwindExplanation.md
@@ -0,0 +1,19 @@
+h1. Hauteur
+* min-h-screen = Set the minimum height of an element using the min-h-0, min-h-full, or min-h-screen utilities.
+
+
+h1. Width
+* w-full = Use w-{fraction} or w-full to set an element to a percentage based width.
+
+
+h1. Hiding content sur overflow
+* overflow-hidden = Use overflow-hidden to clip any content within an element that overflows the bounds of that element.
+
+h1. Ajustement position
+* justify-center : Use justify-center to justify items along the center of the container’s main axis: = aligne au centre selon l'axe abscisses (ou end ou start)
+* items-center : Use items-center to align items along the center of the container’s cross axis: = aligne selon l'axe des ordoonées 
+* flex-initial : Use flex-initial to allow a flex item to shrink but not grow, taking into account its initial size:
+
+
+h1. padding
+* p{t|r|b|l|x|y|empty}-{size} = padding top/right/bottom/left/x axis/y axis /empty : all side
