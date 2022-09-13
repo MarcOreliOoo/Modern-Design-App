de9cdec897f52a40e2e05534bd3f4b316bf16eae
MarcOreliOoo <albeaurelien@gmail.com>
2022-09-13 20:50:04 +0200

    upd: hero

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -26,6 +26,16 @@ const Hero = () => (
 			<h1 className='font-poppins font-semibold ss:text-[66px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full'>
 				Payment Method
 			</h1>
+			<p className={`${styles.paragraph} max-w-[470px] mt-5`}>Our team of expertes uses a methodology to...</p>
+		</div>
+		<div>
+			<img src={robot} alt="billing" className='w-[100%] h-[100%] relative z-[5]'/>
+			<div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient' />
+			<div className='absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient' />
+			<div className='absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient' />
+		</div>
+		<div className={``}>
+
 		</div>
 	</section>
 );
