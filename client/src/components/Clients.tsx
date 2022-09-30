e279b85314b12d4d847820b4e3ea383bf4658f9d
MarcOreliOoo <albeaurelien@gmail.com>
2022-09-30 16:13:58 +0200

    core: clients

--- client/src/components/Clients.tsx
+++ client/src/components/Clients.tsx
@@ -5,7 +5,7 @@ const Clients = () => (
 	<section id="clients" className={`${styles.flexCenter} my-4`}>
 		<div className={`${styles.flexCenter} flex-wrap w-full`}>
 			{clients.map((client) => (
-				<div key={client.id} className={`${styles.flexCenter} flex-1 sm:min-w-[192px] min-h-[100px] min-w-[120px] rounded-[20px] m-5 client-card`}>
+				<div key={client.id} className={`${styles.flexCenter} flex-1 min-h-[100px] rounded-[20px] m-5 client-card`}>
 					<img src={client.logo} alt="logo" className='sm:w-[192px] w-[100px] object-contain' />
 				</div>
 			))}
