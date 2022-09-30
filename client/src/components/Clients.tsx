5a85563352efad14349f027b9f565f4d7fd10134
MarcOreliOoo <albeaurelien@gmail.com>
2022-09-30 14:49:42 +0200

    core: testimonials and upd: clients

--- client/src/components/Clients.tsx
+++ client/src/components/Clients.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { clients } from '../constants';
+import styles from '../style';
 
-const Clients = () => {
-  return (
-	<div>Clients</div>
-  )
-}
+const Clients = () => (
+	<section id="clients" className={`${styles.flexCenter} my-4`}>
+		<div className={`${styles.flexCenter} flex-wrap w-full`}>
+			{clients.map((client) => (
+				<div key={client.id} className={`${styles.flexCenter} flex-1 sm:min-w-[192px] min-w-[120px] m-5 client-card`}>
+					<img src={client.logo} alt="logo" className='sm:w-[192px] w-[100px] object-contain' />
+				</div>
+			))}
+		</div>
+	</section>
+)
 
 export default Clients
