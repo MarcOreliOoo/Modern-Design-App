15c5a11c462351133060126b59ba39c395e84c72
MarcOreliOoo <albeaurelien@gmail.com>
2022-09-30 17:21:21 +0200

    init: footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,7 +1,29 @@
-const Footer = () => {
-	return (
-		<h1>Footer</h1>
-	);
-}
+import styles from "../style";
+import { logo } from "../assets";
+import { footerLinks, socialMedia } from "../constants";
 
+const Footer = () => (
+	<section id="footer" className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
+		<div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+			<div className="flex-1 flex flex-col justify-start mr-10">
+				<img src={logo} alt="hoobank" className="w-[266px] h-[72px] object-contain"/>
+				<p className={`${styles.paragraph} mt-4 m-w-[310px]`}>A new way blabla secure and reliable!</p>
+			</div>
+			<div className="flex-col">
+				{footerLinks.map((footerlink) => (
+					<div key={footerlink.key} className="flex flex-col">
+						<h4>{footerlink.title}</h4>
+						<ul>
+							{footerlink.links.map((link, index)=>(
+								<li key={link.name} className="font-poppins font-normal text-[16px] leading-[24px] text-dimWhite cursor-pointer hover:text-secondary">
+									{link.name}
+								</li>
+							))}
+						</ul>
+					</div>
+				))}
+			</div>
+		</div>
+	</section>
+);
 export default Footer;
