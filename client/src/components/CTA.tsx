06760b248cc584605728c1ba0eb65b6b07080f3c
MarcOreliOoo <albeaurelien@gmail.com>
2022-10-03 12:02:18 +0200

    cta: ano paragraph

--- client/src/components/CTA.tsx
+++ client/src/components/CTA.tsx
@@ -7,7 +7,7 @@ const CTA = () => (
 	sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
 		<div className='flex-1 flex flex-col'>
 			<h2 className={`${styles.heading2}`}>Let's try our service now!</h2>
-			<p className={`${styles.paragraph} max-w-[470px] mt-5 font-poppins font-normal`}>blablablazdqdsesgisegisef,fgroksooksegkkseofkkqkddkoeggegsgegse</p>
+			<p className={`${styles.paragraph} max-w-[270px] mt-5 font-poppins font-normal`}>blablablazdqdsesgisegisef,fgroksookseg kkseofkkqkddkoeggegsgegse</p>
 		</div>
 		<div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
 			<Button styles="rounded-[10px]" />
